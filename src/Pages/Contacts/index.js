576233607c5632702e6bed2d983be3b8c33f3e8e
Stanislav Oleshko <st.oleshko@gmail.com>
2021-11-26 12:40:32 +0100

    contacts css changes

--- src/Pages/Contacts/index.js
+++ src/Pages/Contacts/index.js
@@ -4,29 +4,35 @@ import routeContacts from './routes';
 import myPic from 'assets/minePic.jpg';
 
 const Contacts = () => (
-  <div className='contactsPage'>
-    <div className='info'>
-      <div className='name'>Stanislav Oleszko</div>
-      <div className='tel'>
-        <a href='tel:+48791310907'>+48 791 310 907</a>
+  <section className='contactsPage'>
+    <div className='row justify-content-around align-items-center'>
+      <div col-md-6>
+        <div className='info'>
+          <div className='name'>Stanislav Oleszko</div>
+          <div className='tel'>
+            <a href='tel:+48791310907'>+48 791 310 907</a>
+          </div>
+          <div className='email'>
+            <a href='mailto:oleszko@yahoo.com'>oleszko@yahoo.com</a>
+          </div>
+          <div className='position'>FrontEnd Developer</div>
+          <ul className='tech'>
+            <li>HTML5</li>
+            <li>CSS3/Sass</li>
+            <li>ES6</li>
+            <li>
+              <span>React</span>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className='email'>
-        <a href='mailto:oleszko@yahoo.com'>oleszko@yahoo.com</a>
+      <div className='col-md-6'>
+        <div className='image'>
+          <img loading='lazy' src={myPic} width={400} alt={myPic} />
+        </div>
       </div>
-      <div className='position'>FrontEnd Developer</div>
-      <ul className='tech'>
-        <li>HTML5</li>
-        <li>CSS3/Sass</li>
-        <li>ES6</li>
-        <li>
-          <span>React</span>
-        </li>
-      </ul>
     </div>
-    <div className='image'>
-      <img loading='lazy' src={myPic} width={400} alt={myPic} />
-    </div>
-  </div>
+  </section>
 );
 
 export { routeContacts };
